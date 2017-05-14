a5ae8a743bf183c5512e6544b146dd2dac6dc295
Mohit Singh <mohitsinghs@users.noreply.github.com>
2017-05-14 20:18:36 +0530

    Use Buffer.from instead of new Buffer (#21)

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function compile(data, opts) {
 		const env = (opts && opts.env) || new nunjucks.Environment(new nunjucks.FileSystemLoader(file.base), opts);
 
 		try {
-			file.contents = new Buffer(env.renderString(file.contents.toString(), context));
+			file.contents = Buffer.from(env.renderString(file.contents.toString(), context));
 			this.push(file);
 		} catch (err) {
 			this.emit('error', new gutil.PluginError('gulp-nunjucks', err, {fileName: filePath}));
@@ -47,7 +47,7 @@ function precompile(opts) {
 
 		try {
 			options.name = (typeof options.name === 'function' && options.name(file)) || file.relative;
-			file.contents = new Buffer(nunjucks.precompileString(file.contents.toString(), options));
+			file.contents = Buffer.from(nunjucks.precompileString(file.contents.toString(), options));
 			file.path = gutil.replaceExtension(filePath, '.js');
 			this.push(file);
 		} catch (err) {
